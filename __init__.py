4a26b4cb41f5b42d0607a55657a7a213c2043d55
meng2010 <y.meng201011@gmail.com>
2019-07-25 19:42:51 -0400

    init file

---
+++ __init__.py
@@ -0,0 +1,10 @@
+from __future__ import unicode_literals
+
+# ------------------- CREDITS ---------------------------------------------
+__author__ = 'Ying Meng, Jianhai Su'
+__credits__ = 'Ying Meng, Jianhai Su, Pooyan Jamshidi'
+__version__ = '1.0'
+__copyright__ = 'Copyright (c) 2019 UofSC'
+__license__ = ''
+# -------------------------------------------------------------------------
+
