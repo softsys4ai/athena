1323ef3a8baf373c18f62dd75c24b418846dff7b
meng2010 <y.meng201011@gmail.com>
2019-08-09 00:03:38 -0400

    support more datasets

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 """
 Define global configurations.
-@author: Ying Meng (y(dot)meng201011(at)gmail(dot)com
+@author: Ying Meng (y(dot)meng201011(at)gmail(dot)com,
+        Jianhai Su
 """
 import numpy as np
 
@@ -17,18 +18,19 @@ cvDefenseNames=["Majority", "Max"] # strategies used to decide the label across 
 wcDefenseNames=["1s_SM", "EM_SM", "EM_MMV"]
 kmeansResultFoldName="KMeans_result"
 
-
-
 class TRANSFORMATION:
     """
     Define transformation types that are supported.
     """
-    clean = 'clean' # clean image, no transformation is applied.
+    # clean image, no transformation is applied.
+    clean = 'clean'
 
+    # rotation
     rotate90 = 'rotate90'
     rotate180 = 'rotate180'
     rotate270 = 'rotate270'
 
+    # shift/translation
     shift_left = 'shift_left'
     shift_right = 'shift_right'
     shift_up = 'shift_up'
@@ -38,10 +40,12 @@ class TRANSFORMATION:
     shift_bottom_left = 'shift_bottom_left'
     shift_bottom_right = 'shift_bottom_right'
 
+    # flipping
     horizontal_flip = 'horizontal_flip'
     vertical_flip = 'vertical_flip'
     both_flip = 'both_flip'
 
+    # stretch/compress
     affine_vertical_compress = 'affine_vertical_compress'
     affine_vertical_stretch = 'affine_vertical_stretch'
     affine_horizontal_compress = 'affine_horizontal_compress'
@@ -49,29 +53,20 @@ class TRANSFORMATION:
     affine_both_compress = 'affine_both_compress'
     affine_both_stretch = 'affine_both_stretch'
 
+    # morphology
     erosion = 'erosion'
     dilation = 'dilation'
     opening = 'opening'
     closing = 'closing'
     gradient = 'gradient'
 
-    thresh_binary = 'thresh_binary'
-    thresh_mean = 'thresh_mean'
-    thresh_gaussian = 'thresh_gaussian'
-
+    # augmentation
     samplewise_std_norm = 'samplewise_std_norm'
     feature_std_norm = 'feature_std_norm'
     zca_whitening = 'zca_whitening'
     pca_whitening = 'pca_whitening'
 
-    scaling = 'scaling'
-    upsampling = 'upsampling'
-    downsampling = 'downsampling'
-
-    horizontal_shear = 'horizontal_shear'
-    vertical_shear = 'vertical_shear'
-    range_shear = 'range_shear'
-
+    # cartoonify
     cartoon_mean_type1 = 'cartoon_mean_type1'
     cartoon_mean_type2 = 'cartoon_mean_type2'
     cartoon_mean_type3 = 'cartoon_mean_type3'
@@ -80,6 +75,26 @@ class TRANSFORMATION:
     cartoon_gaussian_type2 = 'cartoon_gaussian_type2'
     cartoon_gaussian_type3 = 'cartoon_gaussian_type3'
     cartoon_gaussian_type4 = 'cartoon_gaussian_type4'
+
+    # quantization
+    quant_2clusters = 'quant_2_clusters'
+    quant_4clusters = 'quant_4_clusters'
+    quant_6clusters = 'quant_6_clusters'
+    quant_8clusters = 'quant_8_clusters'
+    quant_12clusters = 'quant_12_clusters'
+    quant_16clusters = 'quant_16_clusters'
+
+    thresh_binary = 'thresh_binary'
+    thresh_mean = 'thresh_mean'
+    thresh_gaussian = 'thresh_gaussian'
+
+    scaling = 'scaling'
+    upsampling = 'upsampling'
+    downsampling = 'downsampling'
+
+    horizontal_shear = 'horizontal_shear'
+    vertical_shear = 'vertical_shear'
+    range_shear = 'range_shear'
 
     ROTATE = [rotate90, rotate180, rotate270]
     SHIFT = [shift_left, shift_right, shift_up, shift_down,
@@ -92,6 +107,11 @@ class TRANSFORMATION:
     AUGMENT = [samplewise_std_norm, feature_std_norm, zca_whitening]
     CARTOONS = [cartoon_mean_type1, cartoon_mean_type2, cartoon_mean_type3, cartoon_mean_type4,
                 cartoon_gaussian_type1, cartoon_gaussian_type2, cartoon_gaussian_type3, cartoon_gaussian_type4]
+    QUANTIZATIONS = [quant_2clusters, quant_4clusters, quant_6clusters, quant_8clusters,
+                     quant_12clusters, quant_16clusters]
+
+    NOISES =[]
+    FILTERS = []
 
     @classmethod
     def supported_types(cls):
@@ -103,6 +123,7 @@ class TRANSFORMATION:
         transformations.extend(TRANSFORMATION.MORPH_TRANS)
         transformations.extend(TRANSFORMATION.AUGMENT)
         transformations.extend(TRANSFORMATION.CARTOONS)
+        transformations.extend(TRANSFORMATION.QUANTIZATIONS)
 
         return transformations
 
@@ -132,8 +153,8 @@ class ATTACK:
     # ---------------------------
     @classmethod
     def get_fgsm_eps(cls):
-        # return [0.25, 0.3, 0.5, 0.1, 0.05, 0.01, 0.005] # full set
-        return [0.01]
+        # return [0.25, 0.3, 0.1, 0.05, 0.01, 0.005] # full set
+        return [0.25] # for test
 
     # ---------------------------
     # i-FGSM/BIM Parameters
@@ -141,21 +162,21 @@ class ATTACK:
     @classmethod
     def get_bim_nbIter(cls):
         # return [1000, 100, 10000, 10, 1, 100000] # full set
-        return [1000]
+        return [100] # for test
 
     @classmethod
     def get_bim_norm(cls):
-        # return [np.inf, 2] # full set
-        return [np.inf]
+        return [np.inf, 2] # full set
+        # return [np.inf]
 
     @classmethod
     def get_bim_eps(cls, order):
         if order == 2:
-            # return [0.5, 1, 0.25, 0.1, 0.05]
-            return [0.5]
+            return [0.5, 1, 0.25, 0.1]
+            # return [0.5, 0.25]
         elif order == np.inf:
-            # return [0.5, 0.25, 0.1, 0.05, 0.025, 0.01, 0.005]
-            return [0.25]
+            return [0.5, 0.25, 0.1, 0.05, 0.01, 0.005]
+            # return [0.005, 0.01, 0.05]
 
     # ----------------------------
     # Deepfool parameters
@@ -170,56 +191,42 @@ class ATTACK:
     # ----------------------------
     @classmethod
     def get_jsma_theta(cls):
-        # return [-1., -0.7, -0.5, -0.3, -0.1, 0.1, 0.3, 0.5, 0,7, 1.] # full set
-        return [0.5]
+        # theta: Perturbation introduced to modified components (can be positive or negative)
+        # return [0.1, 0.3, 0.5, 0,7, 1.] # full set
+        return [0.1]
 
     @classmethod
     def get_jsma_gamma(cls):
-        # return [0.05, 0.1, 0.3, 0.5, 0.7] # full set. Need to double-check the meaning of gamma, values may change later.
-        return [0.5]
+        # gamma: Maximum percentage of perturbed features
+        # return [0.05, 0.1, 0.3, 0.5, 0.7, 1.] # full set.
+        return [0.7, 0.3]
 
     # ----------------------------
     # CW parameters
     # ----------------------------
-    # TODO:
-    CW_ORD = [np.inf, 2, 0]
-    CW_BIN_SEARCH_STEPS = [1, 2, 3, 4, 5]
-    CW_MAXITERATIONS = [10, 50, 100, 200, 300, 500]
-    CW_LEARNING_RATES = [0.01, 0.05, 0.1, 0.25, 0.5]
-    CW_INITIAL_CONST = [5, 10, 50, 75, 100]
+    @classmethod
+    def get_cw_order(cls):
+        # return [2, np.inf, 0] # full set
+        return [2]
+
+    @classmethod
+    def get_cw_maxIter(cls):
+        # return [1, 10, 100, 1000, 10000, 100000] # full set
+        return [100]
 
 class DATA:
     """
     Configuration for data.
     """
-    DATASET = 'mnist'
-    IMG_ROW = 28
-    IMG_COL = 28
-    NB_CLASSES = 10
-    VALIDATION_RATE = 0.2
+    mnist = 'mnist'
+    fation_mnist = 'fmnist'
+    cifar_10 = 'cifar10'
+    cifar_100 = 'cifar100'
 
     @classmethod
-    def info(cls):
-        print('Dataset: {}'.format(DATA.DATASET))
-        print('Image size: rows - {}; cols - {}'.format(DATA.IMG_ROW, DATA.IMG_COL))
-        print('Validation ratio: {}'.format(DATA.VALIDATION_RATE))
-
-    @classmethod
-    def set_dataset(cls, dataset):
-        DATA.DATASET = dataset
-
-    @classmethod
-    def set_img_size(cls, img_rows, img_cols):
-        DATA.IMG_ROW = img_rows
-        DATA.IMG_COL = img_cols
-
-    @classmethod
-    def set_number_classes(cls, nb_classes):
-        DATA.NB_CLASSES = nb_classes
-
-    @classmethod
-    def set_validation_rate(cls, val_rate):
-        DATA.VALIDATION_RATE = val_rate
+    def get_supported_datasets(cls):
+        datasets = [DATA.mnist, DATA.fation_mnist, DATA.cifar_10, DATA.cifar_100]
+        return datasets
 
 class MODEL:
     """
@@ -248,7 +255,7 @@ class MODEL:
         self.EPOCHS = epochs
 
 class MODE:
-    DEBUG = True
+    DEBUG = False
     @classmethod
     def debug_on(self):
         self.DEBUG = True
@@ -257,8 +264,13 @@ class MODE:
         self.DEBUG = False
 
 class PATH:
-    MODEL = 'data/models'
-    ADVERSARIAL_FILE = 'data/adversarial_examples'
+    if MODE.DEBUG:
+        MODEL = 'data/debug/models'
+        # ADVERSARIAL_FILE = 'data/debug/AEs'
+    else:
+        MODEL = 'data/models'
+        # ADVERSARIAL_FILE = 'data/adversarial_examples'
+    ADVERSARIAL_FILE = 'data/debug/AEs'
     FIGURES = 'data/figures'
     RESULTS = 'data/results'
     ANALYSE = 'data/analyse'
--- data.py
+++ data.py
@@ -13,77 +13,61 @@ from config import DATA
 # set random seed for replication
 tf.set_random_seed(1000)
 
-def load_mnist():
-    """
-    Dataset of 60,000 28x28 grayscale images of the 10 digits,
-    along with a test set of 10,000 images.
-    :return:
-    """
-    dataset = DATA()
-    dataset.set_dataset('mnist')
-    dataset.set_img_size(28, 28)
-    dataset.set_number_classes(10)
+def load_data(dataset):
+    assert dataset in DATA.get_supported_datasets()
 
-    (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
-    X_train = X_train.reshape(-1, dataset.IMG_ROW, dataset.IMG_COL, 1)
-    X_test = X_test.reshape(-1, dataset.IMG_ROW, dataset.IMG_COL, 1)
+    if (dataset == DATA.mnist):
+        """
+        Dataset of 60,000 28x28 grayscale images of the 10 digits,
+        along with a test set of 10,000 images.
+        """
+        (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
+    elif (dataset == DATA.fation_mnist):
+        """
+        Dataset of 60,000 28x28 grayscale images of 10 fashion categories,
+        along with a test set of 10,000 images. The class labels are:
+        Label   Description
+        0       T-shirt/top
+        1       Trouser
+        2       Pullover
+        3       Dress
+        4       Coat
+        5       Sandal
+        6       Shirt
+        7       Sneaker
+        8       Bag
+        9       Ankle boot
+        """
+        (X_train, Y_train), (X_test, Y_test) = fashion_mnist.load_data()
+        raise NotImplementedError('{} is not implemented.'.format(dataset.upper()))
+    elif (dataset == DATA.cifar_10):
+        """
+        Dataset of 50,000 32x32 color training images, labeled over 10 categories, and 10,000 test images.
+        """
+        (X_train, Y_train), (X_test, Y_test) = cifar10.load_data()
+    elif (dataset == DATA.cifar_100):
+        (X_train, Y_train), (X_test, Y_test) = cifar100.load_data()
 
-    # cast pixels to floats, normalize to [-0.5, 0.5] range
+    _, img_rows, img_cols, nb_channels = X_test.shape
+    nb_classes = Y_test.shape[1]
+
+    X_train = X_train.reshape(-1, img_rows, img_cols, nb_channels)
+    X_test = X_test.reshape(-1, img_rows, img_cols, nb_channels)
+
+    # cast pixels to floats, normalize to [0, 1] range
     X_train = X_train.astype('float32')
     X_test = X_test.astype('float32')
-    X_train = (X_train / 255) - 0.5
-    X_test = (X_test / 255) - 0.5
+    X_train /= 255.
+    X_test /= 255.
 
     # one-hot-encode the labels
-    Y_train = np_utils.to_categorical(Y_train, dataset.NB_CLASSES)
-    Y_test = np_utils.to_categorical(Y_test, dataset.NB_CLASSES)
+    Y_train = np_utils.to_categorical(Y_train, nb_classes)
+    Y_test = np_utils.to_categorical(Y_test, nb_classes)
 
     # summarize mnist data set
-    print('Dataset(MNIST) Summary:')
+    print('Dataset({}) Summary:'.format(dataset.upper()))
     print('Train set: {}, {}'.format(X_train.shape, Y_train.shape))
     print('Test set: {}, {}'.format(X_test.shape, Y_test.shape))
 
     return (X_train, Y_train), (X_test, Y_test)
 
-def load_fashion_mnist():
-    """
-    Dataset of 60,000 28x28 grayscale images of 10 fashion categories,
-    along with a test set of 10,000 images. The class labels are:
-    Label   Description
-    0       T-shirt/top
-    1       Trouser
-    2       Pullover
-    3       Dress
-    4       Coat
-    5       Sandal
-    6       Shirt
-    7       Sneaker
-    8       Bag
-    9       Ankle boot
-    :return:
-    """
-    dataset = DATA()
-    dataset.set_dataset('f-mnist')
-    dataset.set_img_size(28, 28)
-    dataset.set_number_classes(10)
-
-    (X_train, Y_train), (X_test, Y_test) = fashion_mnist.load_data()
-    X_train = X_train.reshape(-1, dataset.IMG_ROW, dataset.IMG_COL, 1)
-    X_test = X_test.reshape(-1, dataset.IMG_ROW, dataset.IMG_COL, 1)
-
-    # cast pixels to floats, normalize to [-0.5, 0.5] range
-    X_train = X_train.astype('float32')
-    X_test = X_test.astype('float32')
-    X_train = (X_train / 255) - 0.5
-    X_test = (X_test / 255) - 0.5
-
-    # one-hot-encode the labels
-    Y_train = np_utils.to_categorical(Y_train, dataset.NB_CLASSES)
-    Y_test = np_utils.to_categorical(Y_test, dataset.NB_CLASSES)
-
-    # summarize mnist data set
-    print('Dataset(MNIST) Summary:')
-    print('Train set: {}, {}'.format(X_train.shape, Y_train.shape))
-    print('Test set: {}, {}'.format(X_test.shape, Y_test.shape))
-
-    return (X_train, Y_train), (X_test, Y_test)
