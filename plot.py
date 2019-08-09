7753cbe1d021d914d03ab9abbd3f77fe8ae95283
meng2010 <y.meng201011@gmail.com>
2019-08-08 23:16:21 -0400

    update signature

--- plot.py
+++ plot.py
@@ -5,28 +5,38 @@ Implement methods plotting and drawing figures.
 import matplotlib.pyplot as plt
 from config import PATH
 
-def draw_comparisons(title, controls, treatments):
+def draw_comparisons(controls, treatments, title="None"):
     """
     Draw some comparisons of original images and transformed/perturbed images.
     :param controls: the original images
     :param treatments: the transformed or perturbed images
     :return: na
     """
+    img_rows, img_cols, nb_channels = controls.shape[1:4]
     pos = 1
     fig = plt.figure(figsize=(10, 10))
+
+
     fig.suptitle(title)
     cols = 4
     rows = 5
 
     for i in range(1, 11):
-        fig.add_subplot(rows, cols, pos)
-        img_rows, img_cols = controls.shape[1:3]
+        ax1 = fig.add_subplot(rows, cols, pos)
+        ax1.axis('off')
+        ax1.grid(b=None)
+        ax1.set_aspect('equal')
         # show an original image
         plt.imshow(controls[i - 1].reshape(img_rows, img_cols), cmap='gray')
         pos += 1
-        fig.add_subplot(fig.add_subplot(rows, cols, pos))
+        ax2 = fig.add_subplot(fig.add_subplot(rows, cols, pos))
+        ax2.axis('off')
+        ax2.grid(b=None)
+        ax2.set_aspect('equal')
         # show a transformed/perturbed images
         plt.imshow(treatments[i - 1].reshape(img_rows, img_cols), cmap='gray')
         pos += 1
+
+    plt.subplots_adjust(wspace=0.01, hspace=0.05)
     plt.show()
     fig.savefig('{}/{}.jpg'.format(PATH.FIGURES, title))
