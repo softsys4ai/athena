805445384d17bef3015f3b139bd40ca35fb467f5
meng2010 <y.meng201011@gmail.com>
2019-08-29 21:43:29 -0400

    fixed the indents and added comments

--- file.py
+++ file.py
@@ -23,22 +23,28 @@ def dict2csv(dictionary, file_name):
         writer.writerows(zip(*dictionary.values()))
 
 def save_adv_examples(data, **kwargs):
-    prefix = kwargs.get('prefix', 'test')
-    dataset = kwargs.get('dataset', 'cifar10')
-    architect = kwargs.get('architect', 'cnn')
-    transformation = kwargs.get('transformation', 'clean')
-    attack_method = kwargs.get('attack_method', 'fgsm')
-    attack_params = kwargs.get('attack_params', None)
+  """
+  Serialize generated adversarial examples to an npy file.
+  :param data: the adversarial examples to store.
+  :param kwargs: information needed to construct the file name
+  :return: na
+  """
+  prefix = kwargs.get('prefix', 'test')
+  dataset = kwargs.get('dataset', 'cifar10')
+  architect = kwargs.get('architect', 'cnn')
+  transformation = kwargs.get('transformation', 'clean')
+  attack_method = kwargs.get('attack_method', 'fgsm')
+  attack_params = kwargs.get('attack_params', None)
 
-    prefix = '{}_AE'.format(prefix)
-    attack_info = '{}_{}'.format(attack_method, attack_params)
-    model_info = '{}-{}-{}'.format(dataset, architect, transformation)
-    file_name = '{}-{}-{}.npy'.format(prefix, model_info, attack_info)
-    np.save('{}/{}'.format(PATH.ADVERSARIAL_FILE, file_name), data)
+  prefix = '{}_AE'.format(prefix)
+  attack_info = '{}_{}'.format(attack_method, attack_params)
+  model_info = '{}-{}-{}'.format(dataset, architect, transformation)
+  file_name = '{}-{}-{}.npy'.format(prefix, model_info, attack_info)
+  np.save('{}/{}'.format(PATH.ADVERSARIAL_FILE, file_name), data)
 
-    if MODE.DEBUG:
-        title = '{}-{}'.format(model_info, attack_info)
-        _, (bs_samples, _) = load_data(dataset)
-        draw_comparisons(bs_samples[10:20], data[10:20], title)
+  if MODE.DEBUG:
+      title = '{}-{}'.format(model_info, attack_info)
+      _, (bs_samples, _) = load_data(dataset)
+      draw_comparisons(bs_samples[10:20], data[10:20], title)
 
-    print('Adversarial examples saved to {}/{}.'.format(PATH.ADVERSARIAL_FILE, file_name))
+  print('Adversarial examples saved to {}/{}.'.format(PATH.ADVERSARIAL_FILE, file_name))
