510a0701f980ac15852f779581c71f68db803150
meng2010 <y.meng201011@gmail.com>
2019-08-29 21:26:45 -0400

    a task script to craft adversarial examples

--- file.py
+++ file.py
@@ -6,6 +6,10 @@ Implement file operations such as read/write files here.
 import csv
 import os
 
+from config import *
+from data import load_data
+from plot import draw_comparisons
+
 def dict2csv(dictionary, file_name):
     """
     Serialize values in given dictionary to a csv file.
@@ -18,3 +22,23 @@ def dict2csv(dictionary, file_name):
         writer.writerow(dictionary.keys())
         writer.writerows(zip(*dictionary.values()))
 
+def save_adv_examples(data, **kwargs):
+    prefix = kwargs.get('prefix', 'test')
+    dataset = kwargs.get('dataset', 'cifar10')
+    architect = kwargs.get('architect', 'cnn')
+    transformation = kwargs.get('transformation', 'clean')
+    attack_method = kwargs.get('attack_method', 'fgsm')
+    attack_params = kwargs.get('attack_params', None)
+
+    prefix = '{}_AE'.format(prefix)
+    attack_info = '{}_{}'.format(attack_method, attack_params)
+    model_info = '{}-{}-{}'.format(dataset, architect, transformation)
+    file_name = '{}-{}-{}.npy'.format(prefix, model_info, attack_info)
+    np.save('{}/{}'.format(PATH.ADVERSARIAL_FILE, file_name), data)
+
+    if MODE.DEBUG:
+        title = '{}-{}'.format(model_info, attack_info)
+        _, (bs_samples, _) = load_data(dataset)
+        draw_comparisons(bs_samples[10:20], data[10:20], title)
+
+    print('Adversarial examples saved to {}/{}.'.format(PATH.ADVERSARIAL_FILE, file_name))
