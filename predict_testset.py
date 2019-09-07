0f858cfac15c8ba428d175c608cc0fd9f3c4f797
Jianhai Su <suj@email.sc.edu>
2019-09-07 17:08:33 +0000

    make predict_testset.py to check the accuracies of each sample set on eahc model

--- predict_testset.py
+++ predict_testset.py
@@ -26,24 +26,26 @@ datasetName         = sys.argv[6]
 numOfClasses        = int(sys.argv[7])
 sampleType          = sys.argv[8]
 
+DATA.set_current_dataset_name(datasetName)
+AETypes = ATTACK.get_AETypes()
+
 # Basic parameters for k-fold experiment setup
 architecture = MODEL.ARCHITECTURE
 testDir = os.path.join(experimentRootDir, testResultFoldName)
 createDirSafely(testDir)
 
-sampleTypes =[sampleType]
+sampleTypes =["BS"]
+sampleTypes.extend(AETypes)
+print("[sample types]: {}\n".format(len(sampleTypes)))
+print(sampleTypes)
 
 targetModelName = "clean"
 transformConfig = TRANSFORMATION()
 transformationList = transformConfig.supported_types() 
-#transformationList = ["clean"]
-
-#sampleTypes=["BS", "jsma_theta50_gamma70", "fgsm_eps50"]
-sampleTypes=["fgsm_eps50"]
 
 predictionResultDir = os.path.join(testDir, "prediction_result")
 createDirSafely(predictionResultDir)
-'''
+
 # Prediction : needs a new prediction function
 predictionForTest0(
         predictionResultDir,
@@ -56,12 +58,10 @@ predictionForTest0(
         numOfSamples,
         sampleTypes,
         transformationList)
-'''
-sampleTypes=["BS", "jsma_theta50_gamma70", "fgsm_eps50"]
 
 labels = np.load(os.path.join(samplesDir, "Label-"+datasetName+"-clean.npy"))
 labels = np.argmax(labels, axis=1)
-'''
+
 for sampleType in sampleTypes:
     predDir = os.path.join(predictionResultDir, sampleType)
     predProb = np.load(os.path.join(predDir, "predProb.npy"))
@@ -69,7 +69,7 @@ for sampleType in sampleTypes:
     with open(os.path.join(predictionResultDir, sampleType+"-accuracy.txt"), "w") as fp:
         for i in range(len(transformationList)):
             fp.write("{}\t{}\t{}\n".format(i, transformationList[i], modelsAcc[i]))
-'''
+
 
 with open(os.path.join(predictionResultDir, "singleModelAccuracy.txt"), "w") as fp:
     nST = len(sampleTypes)
