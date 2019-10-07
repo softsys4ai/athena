cfde89bad3df5cea2bda0521a55291a8085aafbc
meng2010 <y.meng201011@gmail.com>
2019-10-06 22:58:18 -0400

    scatter

--- utils/plot.py
+++ utils/plot.py
@@ -3,18 +3,33 @@ Implement methods plotting and drawing figures.
 @author: Ying Meng (y(dot)meng201011(at)gmail(dot)com))
         Jianhai Su
 """
-import os
 import numpy as np
+from utils.csv_headers import IdealModelEvalHeaders as headers
+import os
 import matplotlib.pyplot as plt
-from config import PATH, MODE
+from utils.config import PATH, MODE
 
-line_styles = ['-', '--', '-.', ':']
-colors = ['forestgreen', 'blue', 'magenta', 'mediumspringgreen', 'darkorange', 'red', 'deeppink',
-          'limegreen', 'deepskyblue',  'fuchsia', 'orangered',  'darkgray', 'aqua']
-markers = ['.', 'o', 'v', '^', '<', '>', '+', 's', 'p', '*', 'h', 'x', ',',
+line_styles = ['-', '--', ':', '-.']
+colors = [ 'magenta', 'deepskyblue', 'mediumspringgreen', 'red',
+            'deeppink', 'darkorange', 'fuchsia', 'forestgreen', 'blue',
+           'aqua', 'orangered', 'limegreen', 'darkgray', 'orange', 'black']
+marks = ['o', 's', 'D', '+', '*', 'v', '^', '<', '>', '.', '+', 'p', 'h',  ',',
            'd', '|', '1', '2', '3', '4', '8', 'P', 'H', 'X', 'D']
 nb_colors = len(colors)
-nb_markers = len(markers)
+nb_marks = len(marks)
+
+class LEGEND_LOCATION(object):
+    best = 'best'
+    upper_right = 'upper right'
+    upper_left = 'upper left'
+    lower_left = 'lower left'
+    lower_right = 'lower right'
+    right = 'right'
+    center_left = 'center left'
+    center_right = 'center right'
+    lower_center = 'lower center'
+    upper_center = 'upper center'
+    center = 'center'
 
 def plot_difference(controls, treatments, title="None", save=False):
     """
@@ -129,7 +144,7 @@ def plot_comparisons(controls, treatments, title="None", save=False):
     plt.show()
     plt.close()
 
-def plot_curves(data, title='curves', ylabel='Accuracy', save=False, legend_loc='lower right'):
+def plot_lines(data, title='curves', ylabel='Accuracy', save=False, legend_loc=LEGEND_LOCATION.best):
     """
     Plot curves in one figure, values[keys[i]] vs. values[keys[0]], where i > 0.
     Usage:
@@ -140,15 +155,16 @@ def plot_curves(data, title='curves', ylabel='Accuracy', save=False, legend_loc=
     :param title: string. Title of the figure.
     :param ylabel: string. The label of y-axis.
     :param save: boolean. Save the figure or not.
+    :param legend_loc: location of legend
     :return:
     """
     nb_dimensions = len(data.keys())
     keys = list(data.keys())
 
     for i in range(1, nb_dimensions):
-        m_id = i % nb_markers
-        c_id = i % nb_colors
-        m = '{}{}'.format(line_styles[0], markers[m_id])
+        m_id = (i - 1) % nb_marks
+        c_id = (i - 1) % nb_colors
+        m = '{}{}'.format(line_styles[0], marks[m_id])
         plt.plot(data[keys[0]], data[keys[i]], m, color=colors[c_id], label=keys[i])
 
     plt.title(title)
@@ -164,6 +180,75 @@ def plot_curves(data, title='curves', ylabel='Accuracy', save=False, legend_loc=
 
     plt.show()
     plt.close()
+
+def plot_scatter_with_certainty(data, certainty_borders, title='lines',
+                                ylabel='None', save=False, legend_loc=LEGEND_LOCATION.best):
+    """
+    Plot lines and some filled areas.
+    :param data: dictionary. data used to plot lines.
+    :param certainty_borders: list of tuples. each tuple consists of a sequence of values and corresponding certainties.
+    :param title:
+    :param ylabel:
+    :param save:
+    :param legend_loc: location of legend.
+    :return:
+    """
+    nb_dimensions = len(data.keys())
+    keys = list(data.keys())
+
+    print('keys: ', keys)
+    print('keys[0]: ', data[keys[0]])
+    # plot lines
+    for i in range(1, nb_dimensions):
+        m_id = (i - 1) % nb_marks
+        c_id = (i - 1) % nb_colors
+        mark = '{}{}'.format(line_styles[0], marks[m_id])
+        alpha = 0.9
+        if (headers.GAP.value == keys[i]):
+            mark = line_styles[-1]
+            alpha = 0.6
+
+        plt.plot(data[keys[i]], mark, markerfacecolor='white',
+                 color=colors[c_id], label=keys[i], alpha=alpha, markersize=4)
+
+    # fill areas
+    nb_certainty_areas = len(certainty_borders)
+    for i in range(nb_certainty_areas):
+        x, upper_bound, lower_bound = certainty_borders[i]
+        x1 = [a - 1 for a in x]
+        # x = np.arange(0.0, 73, 1.)
+        print('upper_bound:', upper_bound)
+        print('lower_bound:', lower_bound)
+
+        plt.fill_between(x1, lower_bound, upper_bound, color=colors[-1], alpha=.25)
+
+    plt.title(title, y=1.06, fontsize=14)
+
+    xticks = []
+    xticks_labels = []
+    for i in data[keys[0]]:
+        i = int(i) + 1
+        if 0 == i % 5:
+            xticks.append(i - 1)
+            xticks_labels.append(i)
+
+    plt.xticks(xticks, xticks_labels, fontsize=11)
+    plt.yticks(fontsize=11)
+    plt.xlabel(keys[0].replace('_', ' '), fontsize=14)
+    plt.ylabel(ylabel, fontsize=14)
+    plt.ylim(0.0, 1.08)
+    plt.xlim(0.0, len(data[keys[0]]) - 1)
+    plt.legend(loc=legend_loc, bbox_to_anchor=(0.5, 1.05), ncol=4, fancybox=True, shadow=True)
+
+    if save:
+        plt.savefig(
+            os.path.join(PATH.FIGURES, '{}.pdf'.format(title)),
+            bbox_inches='tight'
+        )
+
+    plt.show()
+    plt.close()
+
 
 def plot_training_history(history, model_name):
     fig = plt.figure(figsize=(1, 2))
