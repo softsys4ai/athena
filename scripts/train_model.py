31b85955b448e9da501b6642b44318562e4beb2c
meng2010 <y.meng201011@gmail.com>
2019-11-12 22:10:02 -0500

    set default transformation_type

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -12,7 +12,7 @@ from data import load_data
 from transformation import transform
 
 
-def train_model(data, transformation_type):
+def train_model(data, transformation_type=TRANSFORMATION.clean):
     X, Y = data
 
     print('Transforming training data set [{}]...'.format(transformation_type))
@@ -28,7 +28,7 @@ def train_model(data, transformation_type):
 
     return model
 
-def test_model(model, test_data, transformation_type):
+def test_model(model, test_data, transformation_type=TRANSFORMATION.clean):
     X, Y = test_data
 
     print('Transforming test data set...')
@@ -52,9 +52,15 @@ def main():
     for transformation_type in trans_types:
         TRANSFORMATION.set_cur_transformation_type(transformation_type)
         try:
-            # model = train_model(training_data, transformation_type)
-            model = models.load_model('model-{}-cnn-{}'.format(DATA.CUR_DATASET_NAME,
-                                                               transformation_type))
+            # step 1. get a model
+            # case 1. train a new model
+            model = train_model(X, transformation_type)
+
+            # or case 2. load an existing model
+            # model = models.load_model('model-{}-cnn-{}'.format(DATA.CUR_DATASET_NAME,
+            #                                                    transformation_type))
+
+            # step 2. estimate the model
             for adversary in adversary_types:
                 X_adv_file = 'test_AE-{}-cnn-clean-{}.npy'.format(DATA.CUR_DATASET_NAME, adversary)
                 print('Evaluating weak defenses on dataset [{}]'.format(X_adv_file))
