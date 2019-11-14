b20c04e35476625ac2a6de707cca168a4a783857
meng2010 <y.meng201011@gmail.com>
2019-11-14 15:31:47 -0500

    evaluation of whitebox and graybox threat models.

--- attacks/attacker.py
+++ attacks/attacker.py
@@ -22,7 +22,7 @@ logger.setLevel(logging.INFO)
 def get_adversarial_examples(model_name, attack_method, X, Y, **kwargs):
     # remove the file format
     model_name = model_name.split('.')[0]
-    dataset = model_name.split('-')[1]
+    dataset = DATA.CUR_DATASET_NAME
 
     if ((not os.path.isfile('{}/{}.h5'.format(PATH.MODEL, model_name))) and
         (not os.path.isfile('{}/{}.json'.format(PATH.MODEL, model_name)))):
@@ -257,7 +257,233 @@ def get_adversarial_examples(model_name, attack_method, X, Y, **kwargs):
 
     print('*** SHAPE: {}'.format(np.asarray(X_adv).shape))
 
-    # del model
-    # sess.close()
+    del model
+    sess.close()
+
+    return X_adv, Y
+
+def attack_whitebox(sess, model, attack_method, X, Y, **kwargs):
+    # remove the file format
+    dataset = DATA.CUR_DATASET_NAME
+
+    logger.info('Crafting adversarial examples using {} method...'.format(attack_method.upper()))
+    X_adv = None
+
+    if (attack_method == ATTACK.FGSM):
+        eps = kwargs.get('eps', 0.25)
+        attack_params = {
+            'eps': eps,
+            'ord': np.inf,
+            'clip_min': 0.,
+            'clip_max': 1.
+        }
+        logger.info('{}: (eps={})'.format(attack_method.upper(), eps))
+
+        start_time = time.time()
+        X_adv, Y = whitebox.generate(sess, model, X, Y, attack_method, dataset, attack_params)
+        duration = time.time() - start_time
+        logger.info('Time cost for generation: {}'.format(duration))
+
+    elif (attack_method == ATTACK.BIM):
+        # iterative fast gradient method
+        eps = kwargs.get('eps', 0.25)
+        nb_iter = kwargs.get('nb_iter', 100)
+        ord = kwargs.get('ord', np.inf)
+
+        """
+        Cleverhans requires an eps_iter that is smaller than the eps. 
+        By default, eps_iter=0.05, so, update eps_iter for small epsilons.
+        """
+        if eps < 0.005:
+            eps_iter = 0.001
+        elif eps < 0.05:
+            eps_iter = 0.005
+        else:
+            # for big enough eps, use the default setting
+            eps_iter = 0.05
+        attack_params = {
+            'eps': eps,
+            'eps_iter': eps_iter,
+            'nb_iter': nb_iter,
+            'ord': ord,
+            'clip_min': 0.,
+            'clip_max': 1.
+        }
+
+        logger.info('{}: (ord={}, nb_iter={}, eps={})'.format(attack_method.upper(), ord, nb_iter, eps))
+        start_time = time.time()
+        X_adv, Y = whitebox.generate(sess, model, X, Y, attack_method, dataset, attack_params)
+
+        duration = time.time() - start_time
+        print('Time cost: {}'.format(duration))
+    elif (attack_method == ATTACK.DEEPFOOL):
+        # Images for inception classifier are normalized to be in [0, 255] interval.
+        # max_iterations = kwargs.get('max_iterations', 100)
+        max_iterations = 100
+        ord = kwargs.get('ord', 2)
+        overshoot = kwargs.get('overshoot', 1.0)
+
+        attack_params = {
+            'ord': ord,
+            'max_iterations': max_iterations,
+            'nb_candidate': Y.shape[1],
+            'overshoot': overshoot,
+            'clip_min': 0.,
+            'clip_max': 1.
+        }
+
+        logger.info('{}: (max_iterations={})'.format(attack_method.upper(), max_iterations))
+        start_time = time.time()
+        X_adv, Y = whitebox.generate(sess, model, X, Y, attack_method, dataset, attack_params)
+        duration = time.time() - start_time
+        print('Time cost: {}'.format(duration))
+
+    elif (attack_method == ATTACK.CW_L2):
+        ord = 2
+        binary_search_steps = kwargs.get('binary_search_steps', 10)
+        batch_size = kwargs.get('cw_batch_size', 2)
+        initial_const = kwargs.get('initial_const', 10)
+        learning_rate = kwargs.get('learning_rate', 0.1)
+        max_iterations = kwargs.get('max_iterations', 100)
+
+        attack_params = {
+            'batch_size': batch_size,
+            'binary_search_steps': binary_search_steps,
+            'initial_const': initial_const,
+            'learning_rate': learning_rate,
+            'max_iterations': max_iterations,
+            'clip_min': 0.,
+            'clip_max': 1.
+        }
+
+        logger.info('{}: (ord={}, max_iterations={})'.format(attack_method.upper(), ord, max_iterations))
+
+        start_time = time.time()
+        X_adv, Y = whitebox.generate(sess, model, X, Y, attack_method, dataset, attack_params)
+        duration = time.time() - start_time
+        logger.info('Time cost: {}'.format(duration))
+
+    elif (attack_method == ATTACK.CW_Linf):
+        ord = np.inf
+
+        decrease_factor = kwargs.get('decrease_factor', 0.9)
+        initial_const = kwargs.get('initial_const', 1e-5)
+        learning_rate = kwargs.get('learning_rate', 0.1)
+        largest_const = kwargs.get('largest_const', 2e+1)
+        max_iterations = kwargs.get('max_iterations', 1000)
+        reduce_const = False
+        const_factor = 3.0
+
+        attack_params = {
+            # 'descrease_factor': decrease_factor,
+            'initial_const': initial_const,
+            'learning_rate': learning_rate,
+            'max_iterations': max_iterations,
+            'largest_const': largest_const,
+            'reduce_const': reduce_const,
+            'const_factor': const_factor,
+            'clip_min': 0.,
+            'clip_max': 1.
+        }
+
+        logger.info('{}: (ord={}, max_iterations={})'.format(attack_method.upper(), ord, max_iterations))
+
+        start_time = time.time()
+        X_adv, Y = whitebox.generate(sess, model, X, Y, attack_method, dataset, attack_params)
+        duration = time.time() - start_time
+        logger.info('Time cost: {}'.format(duration))
+
+    elif (attack_method == ATTACK.CW_L0):
+        max_iterations = kwargs.get('max_iterations', 1000)
+        initial_const = kwargs.get('initial_const', 10)
+        largest_const = kwargs.get('largest_const', 15)
+
+        attack_params = {
+            'max_iterations': max_iterations,
+            'initial_const': initial_const,
+            'largest_const': largest_const
+        }
+
+        X_adv, Y = whitebox.generate(sess, model, X, Y, attack_method, attack_params)
+
+    elif (attack_method == ATTACK.JSMA):
+        theta = kwargs.get('theta', 0.6)
+        gamma = kwargs.get('gamma', 0.5)
+        attack_params = {
+            'theta': theta,
+            'gamma': gamma,
+            'clip_min': 0.,
+            'clip_max': 1.
+        }
+
+        logger.info('{}: (theta={}, gamma={})'.format(attack_method.upper(), theta, gamma))
+        start_time = time.time()
+        X_adv, Y = whitebox.generate(sess, model, X, Y, attack_method, dataset, attack_params)
+        duration = time.time() - start_time
+        logger.info('Time cost: {}'.format(duration))
+
+    elif (attack_method == ATTACK.PGD):
+        eps = kwargs.get('eps', 0.3)
+        nb_iter = kwargs.get('nb_iter', 40)
+        eps_iter = kwargs.get('eps_iter', 0.01)
+
+        attack_params = {
+            'eps': eps,
+            'clip_min': 0.,
+            'clip_max': 1.,
+            'nb_iter': nb_iter,
+            'eps_iter': eps_iter
+        }
+
+        start_time = time.time()
+        X_adv, Y = whitebox.generate(sess, model, X, Y, attack_method, dataset, attack_params)
+        duration = time.time() - start_time
+        logger.info('Time cost: {}'.format(duration))
+
+    elif (attack_method == ATTACK.ONE_PIXEL):
+        # one-pixel was implemented separately.
+        targeted = kwargs.get('targeted', False)
+        pixel_counts = kwargs.get('pixel_counts', 3)
+        max_iter = kwargs.get('max_iter', 10)
+        pop_size = kwargs.get('pop_size', 10)
+
+        attack_params = {
+            'targeted': targeted,
+            'pixel_counts': pixel_counts,
+            'max_iter': max_iter,
+            'pop_size': pop_size,
+            'clip_min': 0.,
+            'clip_max': 1.,
+        }
+
+        start_time = time.monotonic()
+        X_adv, Y = one_pixel.generate(model, X, Y, attack_params)
+        duration = time.monotonic() - start_time
+        logger.info('Time cost: {}'.format(duration))
+
+    elif (attack_method == ATTACK.MIM):
+        eps = kwargs.get('eps', 0.3)
+        eps_iter = kwargs.get('eps_iter', 0.06)
+        nb_iter = kwargs.get('nb_iter', 10)
+        decay_factor = kwargs.get('decay_factor', 1.0)
+        y_target = kwargs.get('y_target', None)
+
+        attack_params = {
+            'eps': eps,
+            'eps_iter': eps_iter,
+            'nb_iter': nb_iter,
+            'ord': np.inf,
+            'decay_factor': decay_factor,
+            'y_target': y_target,
+            'clip_min': 0.,
+            'clip_max':1.
+        }
+
+        start_time = time.time()
+        X_adv, Y = whitebox.generate(sess, model, X, Y, attack_method, dataset, attack_params)
+        duration = time.time() - start_time
+        logger.info('Time cost: {}'.format(duration))
+
+    print('*** SHAPE: {}'.format(np.asarray(X_adv).shape))
 
     return X_adv, Y
---
+++ evaluation/eval_whitebox.py
@@ -0,0 +1,325 @@
+"""
+Implement the evaluation to white-box and gray-box threat models.
+@author: Ying Meng (y(dot)meng201011(at)gmail(dot)com)
+"""
+
+from enum import Enum
+import numpy as np
+import os
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+
+import copy
+import random
+import time
+import tensorflow as tf
+import tensorflow.keras as keras
+
+import attacks.attacker
+import attacks.whitebox
+from attacks.attacker import attack_whitebox
+from data import load_data
+from models import load_model
+from utils.plot import plot_image
+from transformation import transform
+from utils.config import *
+from utils.file import save_adv_examples
+import utils.measure as measure
+
+class ATTACK_STRATEGY(Enum):
+    MOST_EFFECTIVE = 'best'
+    LEAST_EFFECTIVE = 'worst'
+    RANDOM = 'random'
+
+def get_waive_list(dataset=DATA.mnist):
+    if dataset == DATA.mnist:
+        DATA.set_current_dataset_name(DATA.mnist)
+        waives = TRANSFORMATION.QUANTIZATIONS
+        waives.extend([TRANSFORMATION.denoise_nl_means])
+    else:
+        # TODO: add the items later
+        waives = []
+
+    return waives
+
+def get_attack_params(attack_method=ATTACK.FGSM):
+    # TODO: support schedulable attack params
+    attack_params = {
+        'clip_min': 0.,
+        'clip_max': 1.
+    }
+
+    if attack_method == ATTACK.FGSM:
+        attack_params['eps'] = 0.10
+        attack_params['ord'] = np.inf
+    elif attack_method == ATTACK.BIM:
+        attack_params['eps'] = 0.1
+        attack_params['nb_iter'] = 100
+        attack_params['ord'] = 2
+    elif attack_method == ATTACK.DEEPFOOL:
+        attack_params['max_iterations'] = 100
+        attack_params['ord'] = 2
+        attack_params['overshoot'] = 1.0
+    elif attack_method == ATTACK.CW_L2:
+        attack_params['initial_const'] = 10
+        attack_params['learning_rate'] = 0.1
+        attack_params['max_iterations'] = 100
+    elif attack_method == ATTACK.JSMA:
+        attack_params['theta'] = 0.3
+        attack_params['gamma'] = 0.3
+    elif attack_method == ATTACK.PGD:
+        attack_params['eps'] = 0.1
+    elif attack_method == ATTACK.ONE_PIXEL:
+        attack_params['pixel_counts'] = 1
+        attack_params['max_iter'] = 10
+        attack_params['pop_size'] = 10
+    elif attack_method == ATTACK.MIM:
+        attack_params['eps'] = 0.1
+        attack_params['nb_iter'] = 10
+
+    return attack_params
+
+
+def init_candidate_targets(weak_defenses_file):
+    """
+    :return: the list of candidate targets, which are
+            sorted descendingly according to their effectiveness.
+    """
+    # load all models
+    candidates = {}
+    waives = get_waive_list()
+
+    with open(os.path.join(PATH.MODEL, weak_defenses_file), 'r') as weak_defense_list:
+        for weak_defense in weak_defense_list:
+            weak_defense = weak_defense[:-1] # remove the \newline character
+            print('...Loading weak defense {}'.format(weak_defense))
+            name = weak_defense.split('.')[0].split('-')[-1]
+            if name in waives:
+                print('Ignore [{}]'.format(name))
+                continue
+
+            candidates[name] = load_model(weak_defense)
+
+    return candidates
+
+
+def sort_candidates(candidates, attacker):
+    # TODO: sort candidates by their efficiency for certain attacker
+    return candidates
+
+
+def pick_target_model(candidate_names, strategy=ATTACK_STRATEGY.RANDOM.value):
+    """
+    :param candidate_names: the list of candidate targets, sorted by their effectiveness
+    :param strategy: strategy to choose a target
+    :return: a target model
+    """
+    if strategy == ATTACK_STRATEGY.MOST_EFFECTIVE.value:
+        return candidate_names[0]
+    elif strategy == ATTACK_STRATEGY.LEAST_EFFECTIVE.value:
+        return candidate_names[-1]
+    elif strategy == ATTACK_STRATEGY.RANDOM.value:
+        name = random.choice(candidate_names)
+        print('>>> Pick target [{}]'.format(name))
+        return name
+
+
+def reset(X, trans_type):
+    if trans_type == TRANSFORMATION.rotate90:
+        X = transform(X, TRANSFORMATION.rotate270)
+    elif trans_type == TRANSFORMATION.rotate270:
+        X = transform(X, TRANSFORMATION.rotate90)
+    elif trans_type == TRANSFORMATION.rotate180:
+        X = transform(X, TRANSFORMATION.rotate180)
+
+    return X
+
+def get_perturb_upperbound(attacker=ATTACK.FGSM):
+    orig_file = 'test_BS-{}-clean.npy'.format(DATA.CUR_DATASET_NAME)
+    orig_file = os.path.join(PATH.ADVERSARIAL_FILE, orig_file)
+
+    adv_file = 'test_AE-{}-cnn-clean'.format(DATA.CUR_DATASET_NAME)
+    adv_file = os.path.join(PATH.ADVERSARIAL_FILE, adv_file)
+
+    if attacker == ATTACK.FGSM:
+        # use the strongest as upperbound
+        adv_file = '{}-{}.npy'.format(adv_file, ATTACK.get_fgsm_AETypes()[-1])
+    elif attacker == ATTACK.JSMA:
+        adv_file = '{}-{}.npy'.format(adv_file, ATTACK.get_jsma_AETypes()[-1])
+
+    print('Loading original file [{}]...'.format(orig_file))
+    X = np.load(orig_file)
+    print('Loading perturbed file [{}]...'.format(adv_file))
+    X_adv = np.load(adv_file)
+    upperbound = np.round(measure.frobenius_norm(X1=X_adv, X2=X), 2)
+
+    print('Upperbound of {} perturbation: {}'.format(attacker, upperbound))
+    return upperbound
+
+
+def attack_single(sess, target_model, attacker, x, y, **attack_params):
+    if len(x.shape) < 4:
+        x = np.expand_dims(x, axis=0)
+
+    if len(y.shape) < 4:
+        y = np.expand_dims(y, axis=0)
+
+    x_adv, _ =  attack_whitebox(sess, target_model, attacker, x, y, **attack_params)
+
+    return x_adv
+
+
+def generate_single(sess, x, y, attacker=ATTACK.FGSM,
+                    candidates=None,
+                    attack_count=None,
+                    max_perturb=get_perturb_upperbound(),
+                    strategy=ATTACK_STRATEGY.RANDOM.value):
+    # candidate_names = candidates.copy()
+    candidate_names = copy.deepcopy(list(candidates.keys()))
+    fooled = []
+
+    attack_params = get_attack_params(attacker)
+    x_adv = x
+    perturbed_norm = measure.frobenius_norm(x_adv, x)
+
+    max_iteration = len(candidate_names)
+    iter = 0
+    while ((len(fooled) < attack_count) and
+           (iter < max_iteration)):
+        # generate adversarial example for target model
+        print('ITERATION {}: candidates/fooled ::: {}/{}'.format(iter, len(candidate_names), len(fooled)))
+        iter += 1
+        target_name = pick_target_model(candidate_names, strategy)
+        transformation = target_name.split('.')[0].split('-')[-1]
+        x_trans = transform(x_adv, transformation)
+        if len(x_trans.shape) < 4:
+            print('x_trans shape:', x_trans.shape)
+            x_trans = np.expand_dims(x_trans, axis=0)
+
+        x_tmp = attack_single(sess, candidates[target_name], attacker, x_trans, y, **attack_params)
+        perturbed_norm = measure.frobenius_norm(x_tmp, transform(x, transformation))
+        if perturbed_norm >= max_perturb:
+            # keep the last x_adv if current one is out of the boundary
+            print('out of perturbed boundary, stop.')
+            break
+
+        x_adv = reset(x_tmp, transformation)
+
+        if MODE.DEBUG:
+            plot_image(x_adv[0], transformation)
+
+        del x_trans
+
+        # filter out candidates that are fooled by x_adv
+        true_label = np.argmax(y)
+        for cand_name in candidate_names:
+            transformation = cand_name.split('.')[0].split('-')[-1]
+
+            # apply transformation
+            x_trans = transform(x_adv, transformation)
+            pred_label = np.argmax(candidates[cand_name].predict(x_trans))
+
+            if MODE.DEBUG:
+                print('prediction: [{}/{}/{}]'.format(transformation, true_label, pred_label))
+
+            if (true_label != pred_label):
+                # remove candidate being fooled by x_adv
+                candidate_names.remove(cand_name)
+                # record only the name of the weak defense
+                print('+++ fooled [{}]'.format(cand_name))
+                fooled.append(cand_name)
+            # release
+            del x_trans
+
+        # use current adversarial example as the input of next iteration
+        print('')
+        del target_name
+
+    return x_adv[0]
+
+def gen_greedy(dataset, attacker=ATTACK.FGSM,
+               attack_count=None, strategy=ATTACK_STRATEGY.RANDOM.value):
+
+    config = tf.ConfigProto(intra_op_parallelism_threads=4,
+                            inter_op_parallelism_threads=4)
+    sess = tf.Session(config=config)
+    keras.backend.set_session(sess)
+
+    candidates = init_candidate_targets('ensemble/mnist_weak_defenses_fsgm.list')
+
+    print('...In total {} weak defenses.'.format(len(candidates)))
+
+    prefix = 'wb' # white-box
+
+    if attack_count == None or attack_count <= 0:
+        prefix = 'gb' # gray-box
+        attack_count = len(candidates.keys())
+
+    X_adv = []
+
+    _, (X, Y) = load_data(dataset=dataset)
+
+    # generate 500 samples
+    batch_size = 100
+    nb_samples = Y.shape[0]
+    nb_iter = int(nb_samples / batch_size)
+
+    start = time.monotonic()
+    for i in range(nb_iter):
+        start_idx = i * batch_size
+        end_idx = min((i + 1) * batch_size, nb_samples)
+        print(start_idx, end_idx)
+        X_batch = X[start_idx:end_idx]
+        Y_batch = Y[start_idx:end_idx]
+
+        print('...In total {} inputs.'.format(Y.shape[0]))
+        idx = 0
+        for x, y in zip(X_batch, Y_batch):
+            print('{}-th input...'.format(idx))
+
+            x = np.expand_dims(x, axis=0)
+
+            strategy = ATTACK_STRATEGY.RANDOM.value
+
+            '''
+            generate_single(sess, x, y, attacker=ATTACK.FGSM,
+                        candidates=None,
+                        attack_count=None,
+                        max_perturb=get_perturb_upperbound(),
+                        strategy=ATTACK_STRATEGY.RANDOM.value)
+            '''
+            start_sample = time.monotonic()
+            X_adv.append(generate_single(sess, x, y, attacker, candidates, attack_count, strategy=strategy))
+            end_sample = time.monotonic()
+            print('({}, {})-th sample: {}\n\n'.format(i, idx, (end_sample - start_sample)))
+            idx += 1
+
+        save_adv_examples(np.asarray(X_adv), prefix=prefix, bs_samples=X_batch, dataset=dataset,
+                          transformation=strategy, attack_method=attacker,
+                          attack_params='eps100_batchsize{}_{}'.format(batch_size, i))
+
+    duration = time.monotonic() - start
+    print('----------------------------------')
+    print('        Summary')
+    print('----------------------------------')
+    print('Number of inputs:', Y.shape[0])
+    print('Adversary:', attacker)
+    print('Strategy:', strategy)
+    print('Time cost:', duration)
+
+    sess.close()
+
+def gen_separately(dataset, attacker=ATTACK.FGSM, attack_count=None,
+                   strategy=ATTACK_STRATEGY.RANDOM.value):
+    target_models = init_candidate_targets(attacker)
+
+
+    pass
+
+def main():
+    DATA.set_current_dataset_name(DATA.mnist)
+    gen_greedy(dataset=DATA.CUR_DATASET_NAME, attacker=ATTACK.FGSM)
+
+if __name__ == '__main__':
+    # MODE.debug_on()
+    MODE.debug_off()
+    main()
