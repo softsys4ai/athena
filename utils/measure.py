6e5d05fef5a52eed5e92716abc25c8872ecdbf17
meng2010 <y.meng201011@gmail.com>
2019-11-14 15:40:49 -0500

    support measuring frobenius norm between two images.

--- utils/measure.py
+++ utils/measure.py
@@ -14,14 +14,20 @@ def frobenius_norm(X1, X2):
     :return:
     """
     ord = 2
-    nb_samples = X1.shape[0]
-    img_rows = X1.shape[1]
-    img_cols = X1.shape[2]
+    if len(X1.shape) < 4:
+        nb_samples = 1
+        img_rows = X1[0]
+        img_cols = X1.shape[1]
+    else:
+        nb_samples = X1.shape[0]
+        img_rows = X1.shape[1]
+        img_cols = X1.shape[2]
 
     norm = 0.
     for x1, x2 in zip(X1, X2):
+
         x1 = x1.reshape(img_rows, img_cols)
-        x2 = x1.reshape(img_rows, img_cols)
+        x2 = x2.reshape(img_rows, img_cols)
         perturbation = abs(x1 - x2)
         norm += LA.norm(perturbation, ord=ord) / LA.norm(x2, ord=ord)
 
