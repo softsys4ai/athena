b6bb1355ab220231fac7f6cb0c17810096c323d6
meng2010 <y.meng201011@gmail.com>
2019-12-26 15:07:07 -0500

    update linewidth for boxplot

--- utils/plot.py
+++ utils/plot.py
@@ -163,21 +163,21 @@ def boxplot(data_to_plot, title="Boxplot", xticklabels=None,
     ## change outline color, fill color and linewidth of the boxes
     for box in bp['boxes']:
         # change outline color
-        box.set(color='#7570b3', linewidth=2)
+        box.set(color='#7570b3', linewidth=line_width)
         # change fill color
         box.set(facecolor='#1b9e77')
 
     ## change color and linewidth of the whiskers
     for whisker in bp['whiskers']:
-        whisker.set(color='#7570b3', linewidth=2)
+        whisker.set(color='#7570b3', linewidth=line_width)
 
     ## change color and linewidth of the caps
     for cap in bp['caps']:
-        cap.set(color='#7570b3', linewidth=2)
+        cap.set(color='#7570b3', linewidth=line_width)
 
     ## change color and linewidth of the medians
     for median in bp['medians']:
-        median.set(color='#b2df8a', linewidth=2)
+        median.set(color='#b2df8a', linewidth=line_width)
 
     ## change the style of fliers and their fill
     for flier in bp['fliers']:
