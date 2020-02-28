1d158d8df6e15b0f1448bd30218383f35aef1468
meng2010 <y.meng201011@gmail.com>
2020-02-28 10:20:47 -0500

    1. support complex cnn models (e.g., wresnet for cifar100). 2. add new metrics (topK).

--- src/models/ensemble.py
+++ src/models/ensemble.py
@@ -3,10 +3,12 @@ import time
 import numpy as np
 
 from collections import Counter
+import operator
 from sklearn.metrics import accuracy_score
 from tensorflow.keras.models import load_model, Model
 
 from models.transformation import transform
+import utils.data_utils as data_utils
 
 def load_models(modelsDir, modelFilenamePrefix, transformationList, convertToLogit=False):
     models=[]
@@ -31,7 +33,7 @@ def load_models(modelsDir, modelFilenamePrefix, transformationList, convertToLog
     print("Number of loaded models: {}".format(len(models)))
     return models
 
-def prediction(data, models, nClasses, transformationList):
+def prediction(data, models, nClasses, transformationList, batch_size=32, channel_last=True):
     '''
         input:
             data: nSamples X <Sample Dimension>
@@ -41,25 +43,32 @@ def prediction(data, models, nClasses, transformationList):
     '''
     nSamples, nWeakModels = data.shape[0], len(models)
     rawPred = np.zeros((nWeakModels, nSamples, nClasses))
+
     transTCs = []
     predTCs = []
+    data = np.float32(data)
     for mIdx in range(nWeakModels):
-        testData = data.copy() # some transformation will change the data.
-
         startTime = time.time()
         transformationType = transformationList[mIdx]
-        testData = transform(testData, transformationType)
+        testData = transform(data, transformationType)
         transTCs.append(time.time()-startTime)
 
+        if not channel_last:
+            # input shape of cnn model is <n_samples, n_channels, rows, cols>
+            testData = data_utils.set_channels_first(testData)
         startTime = time.time()
-        rawPred[mIdx] = models[mIdx].predict(testData)
+        rawPred[mIdx] = models[mIdx].predict(testData, batch_size=batch_size)
         predTCs.append(time.time() - startTime)
 
     return rawPred, transTCs, predTCs
 
 
+def get_topK(arr, topK=1):
+    return arr.argsort()[-topK:][::-1]
+
+
 # ensemble_ID = 0
-def ensemble_random_defense(rawPred):
+def ensemble_random_defense(rawPred, topK=1):
     '''
         input:
             rawPred: nWeakModels X nSamples X nClasses
@@ -68,16 +77,18 @@ def ensemble_random_defense(rawPred):
     '''
     inputShape = rawPred.shape
     nSamples, nWeakModels = inputShape[1], inputShape[0] 
-    predLabels = np.array([-1]*nSamples)
+    predLabels = []
 
     for sIdx in range(nSamples):
         weakModelIdx = np.random.choice(nWeakModels)
-        predLabels[sIdx] = np.argmax(rawPred[weakModelIdx, sIdx, :])
+        preds = np.array(rawPred[weakModelIdx, sIdx, :])
+        predLabels.append(get_topK(preds, topK))
 
-    return predLabels
+    return np.asarray(predLabels)
+
 
 # ensemble_ID = 1
-def ensemble_majority_voting(rawPred):
+def ensemble_majority_voting(rawPred, topK=1):
     '''
         input:
             rawPred: nWeakModels X nSamples X nClasses
@@ -86,20 +97,20 @@ def ensemble_majority_voting(rawPred):
     '''
     inputShape = rawPred.shape
     nSamples, nWeakModels = inputShape[1], inputShape[0]
-    predLabels = np.array([-1]*nSamples)
+    predLabels = []
 
     for sIdx in range(nSamples):
         labels = np.argmax(rawPred[:, sIdx, :], axis=1)
-        c = Counter(labels.tolist())
-        majorityVote, count = c.most_common()[0]
-        predLabels[sIdx] = majorityVote
+        c = sorted(Counter(labels.tolist()).items(), key=operator.itemgetter(1), reverse=True)
+        labels = np.array(k[0] for k in c)
+        predLabels.append(labels[:topK])
 
-    return predLabels
+    return np.asarray(predLabels)
 
 # ensemble_ID = 2
 # confidence: probability or logit
 # derive two ensemble models
-def ensemble_ave_confidence(rawPred):
+def ensemble_ave_confidence(rawPred, topK=1):
     '''
         input:
             rawPred: nWeakModels X nSamples X nClasses
@@ -108,17 +119,17 @@ def ensemble_ave_confidence(rawPred):
     '''
     inputShape = rawPred.shape
     nSamples, nWeakModels = inputShape[1], inputShape[0]
-    predLabels = np.array([-1]*nSamples)
+    predLabels = []
 
     for sIdx in range(nSamples):
         means = rawPred[:, sIdx, :].mean(axis=0)
-        predLabels[sIdx] = np.argmax(means)
+        predLabels.append(get_topK(means, topK))
 
-    return predLabels
+    return np.asarray(predLabels)
 
 
 # ensemble_ID = 3
-def ensemble_top2labels_majority_voting(rawPred):
+def ensemble_top2labels_majority_voting(rawPred, topK=1):
     '''
         input:
             rawPred: nWeakModels X nSamples X nClasses
@@ -127,18 +138,18 @@ def ensemble_top2labels_majority_voting(rawPred):
     '''
     inputShape = rawPred.shape
     nSamples, nWeakModels = inputShape[1], inputShape[0]
-    predLabels = np.array([-1]*nSamples)
+    predLabels = []
 
     for sIdx in range(nSamples):
         ind = np.argpartition(rawPred[:, sIdx, :], -2, axis=1)[:, -2:]
         labels = np.ravel(ind)
-        c = Counter(labels.tolist())
-        majorityVote, count = c.most_common()[0]
-        predLabels[sIdx] = majorityVote
+        c = sorted(Counter(labels.tolist()).items(), key=operator.itemgetter(1), reverse=True)
+        labels = np.array([k[0] for k in c])
+        predLabels.append(labels[:topK])
 
-    return predLabels
+    return np.asarray(predLabels)
 
-def ensemble_defenses_util(rawPred, ensembleID):
+def ensemble_defenses_util(rawPred, ensembleID, topK=1):
     '''
         input:
             rawPred: nWeakModels X nSamples X nClasses
@@ -148,13 +159,13 @@ def ensemble_defenses_util(rawPred, ensembleID):
     '''
 
     if ensembleID == 0:
-        return ensemble_random_defense(rawPred)
+        return ensemble_random_defense(rawPred, topK)
     elif ensembleID == 1:
-        return ensemble_majority_voting(rawPred)
+        return ensemble_majority_voting(rawPred, topK)
     elif ensembleID == 2:
-        return ensemble_ave_confidence(rawPred)
+        return ensemble_ave_confidence(rawPred, topK)
     elif ensembleID == 3:
-        return ensemble_top2labels_majority_voting(rawPred)
+        return ensemble_top2labels_majority_voting(rawPred, topK)
 
 def ensemble_defenses(
         modelsDir,
@@ -180,7 +191,7 @@ def ensemble_defenses(
     models = load_models(modelsDir, modelFilenamePrefix, transformationList, convertToLogit=convertToLogit)
 
     data = np.load(datasetFilePath)
-    data = np.clip(data, 0, 1) # ensure its values inside [0, 1]
+    data = data_utils.rescale(data) # ensure its values inside [0, 1]
     rawPred, transTCs, predTCs = prediction(data, models, nClasses, transformationList)
 
     return ensemble_defenses_util(rawPred, ensembleID)
@@ -211,8 +222,8 @@ def evaluate_ensemble_defenses(
             nClasses,
             ensembleID,
             useLogit=useLogit,
-            checkTimeCost=False)
+            checkTimeCost=checkTimeCost)
 
-    trueLabels = np.load(trueLabelPath)
+    trueLabels = np.load(trueLabelFilePath)
 
     return round(accuracy_score(trueLabels, predLabels), 4)
