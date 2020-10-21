9ec74b3f650a78bf6466f2d9f6db82a28a29e016
meng2010 <y.meng201011@gmail.com>
2020-10-21 04:14:33 -0400

    remove scripts (only website related files on this branch)

--- src/__init__.py
+++
@@ -1,9 +0,0 @@
-from __future__ import unicode_literals
-
-# ------------------- CREDITS ---------------------------------------------
-__author__ = 'Ying Meng, Jianhai Su'
-__credits__ = 'Ying Meng, Jianhai Su, Pooyan Jamshidi'
-__version__ = '1.2'
-__copyright__ = 'Copyright (c) 2019 UofSC'
-__license__ = 'MIT'
-# -------------------------------------------------------------------------
