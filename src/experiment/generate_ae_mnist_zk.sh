9bfb343634603a5f93df8078b22150e91cceb6ee
MENG <y.meng201011@gmail.com>
2021-09-19 00:49:44 -0400

    Delete generate_ae_mnist_zk.sh

--- src/experiment/generate_ae_mnist_zk.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Generating adversarial examples in the context of the zero-knowledge model..."
-python craft_ae_mnist_cnn.py --experiment-root "../../../" --pool-configs "../configs/experiment/mnist/vanilla-athena.json" --model-configs "../configs/experiment/mnist/model-info.json" --data-configs "../configs/experiment/mnist/data-info.json" --attack-configs "../configs/experiment/mnist/attack-demo.json" --targeted-model "single" --eot "False" --output-root "../../experiment/mnist/results"
